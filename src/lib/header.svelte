56ab2cf8fa4b5ef4c52107f8ce649175802ec703
Moustapha Diaby <me@isaacdiaby.tech>
2023-12-08 02:50:55 +0000

    made the unauthed view the same

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -2,14 +2,10 @@
 	import {
 		Button,
 		Modal,
-		Label,
-		Input,
-		Checkbox,
 		Dropdown,
 		DropdownItem,
 		Avatar,
-		DropdownHeader,
-		DropdownDivider,
+        Label
 	} from "flowbite-svelte";
 
 	import Auth from "./Auth.svelte";
@@ -20,7 +16,6 @@
 	let user;
 	supabase.auth.onAuthStateChange((event, session) => {
 		if (event === "INITIAL_SESSION") {
-			user = session.user;
 			console.log("Welcome to Colour-Tile!");
 			console.log("Auth Status:", session.user);
 			// TODO: New user onboarding walk through?
@@ -57,49 +52,50 @@
 			<ul
 				class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
 			>
-				{#if user == null}
-					<li>
-						<Button
-							on:click={() => (authModal = true)}
-							class=""
-							aria-current="page">Login</Button
-						>
-					</li>
-				{:else}
-					<!-- Authed  -->
+				<Button pill color="light" id="avatar_menu" class="!p-1">
+					<Avatar
+						src={user?.user_metadata.picture ?? ""}
+						class="me-2"
+					/>
+                    <Label defaultClass="text-lg" >
+					{user?.user_metadata.full_name ?? "👋"}
+                </Label>
+				</Button>
 
-					<Button
-						pill
-						color="light"
-						id="avatar_menu"
-						class="!p-1"
-					>
-						<Avatar src={user.user_metadata.picture} class="me-2" />
-						{user.user_metadata.full_name}
-					</Button>
-					<Dropdown triggeredBy="#avatar_menu">
-						<div slot="header" class="px-4 py-2">
+				<Dropdown triggeredBy="#avatar_menu">
+					<div slot="header" class="px-4 py-2">
+						{#if user == null}
 							<span
 								class="block text-sm text-gray-900 dark:text-white"
-								>{user.user_metadata.full_name}</span
+								>{user?.user_metadata.full_name ?? ""}</span
 							>
 							<span class="block truncate text-sm font-medium"
-								>{user.user_metadata.email}</span
+								>{user?.user_metadata.email ?? ""}</span
 							>
-						</div>
+						{/if}
+					</div>
+					<DropdownItem on:click={() => (leaderboardModal = true)}
+						>Leaderboard</DropdownItem
+					>
+					<DropdownItem>Solve</DropdownItem>
+					<DropdownItem>Restart</DropdownItem>
+					{#if user == null}
 						<DropdownItem
-                        on:click={() => (leaderboardModal = true)}
-                        >Leaderboard</DropdownItem>
-						<DropdownItem>Solve</DropdownItem>
-						<DropdownItem>Restart</DropdownItem>
-						<DropdownItem slot="footer" on:click={() => {
-                            supabase.auth.signOut();
-                        }}>Sign out</DropdownItem>
-					</Dropdown>
+							slot="footer"
+							on:click={() => (authModal = true)}
+							>Login</DropdownItem
+						>
+					{:else}
+						<DropdownItem
+							slot="footer"
+							on:click={() => {
+								supabase.auth.signOut();
+							}}>Sign out</DropdownItem
+						>
+					{/if}
+				</Dropdown>
 
-
-				{/if}
-
+				<!-- {/if} -->
 			</ul>
 		</div>
 	</div>
